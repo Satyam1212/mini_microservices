0beba2948ed55012d617cc611b4032d42c185323
Satyam1212 <satyamjadhav088@gmail.com>
2024-03-08 00:06:02 +0530

    Containerize event-bus and posts service

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,21 +14,21 @@ app.post('/events', (req, res) => {
     console.log('Received Event:', event); // Log received event
 
     // Forward the event to other services
-    axios.post('http://localhost:4000/events', event)
+    axios.post('http://posts-clusterip-srv:4000/events', event)
         .then(() => console.log('Event forwarded to Post Service'))
         .catch((err) => console.error('Error forwarding event to Post Service:', err.message));
 
-    axios.post('http://localhost:4001/events', event)
-        .then(() => console.log('Event forwarded to Comment Service'))
-        .catch((err) => console.error('Error forwarding event to Comment Service:', err.message));
+    // axios.post('http://localhost:4001/events', event)
+    //     .then(() => console.log('Event forwarded to Comment Service'))
+    //     .catch((err) => console.error('Error forwarding event to Comment Service:', err.message));
 
-    axios.post('http://localhost:4002/events', event)
-        .then(() => console.log('Event forwarded to Query Service'))
-        .catch((err) => console.error('Error forwarding event to Query Service:', err.message));
+    // axios.post('http://localhost:4002/events', event)
+    //     .then(() => console.log('Event forwarded to Query Service'))
+    //     .catch((err) => console.error('Error forwarding event to Query Service:', err.message));
 
-    axios.post('http://localhost:4003/events', event)
-        .then(() => console.log('Event forwarded to Moderation Service'))
-        .catch((err) => console.error('Error forwarding event to Moderation Service:', err.message));
+    // axios.post('http://localhost:4003/events', event)
+    //     .then(() => console.log('Event forwarded to Moderation Service'))
+    //     .catch((err) => console.error('Error forwarding event to Moderation Service:', err.message));
 
     events.push(event); // Add the event to the events array
     res.send({ status: 'OK' });
